1b2e25b53f27741147d715d104f504564e5f9b61
olgajuanne <olgajdango@gmail.com>
2023-10-10 19:55:05 -0300

    Fundamentos de java

---
+++ src/main/java/com/olganunes/todolist/user/UserController.java
@@ -0,0 +1,51 @@
+package com.olganunes.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+        var userCreated =  this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+  
+}
+
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiros
+     * Double (double) Números 0.0000
+     * Float (float) Números 0.000
+     * char (A C)
+     * Date(data)
+     * void (quando não tem retorno)
+     */
+    /*
+     * Body
+     */
